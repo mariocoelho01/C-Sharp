8f724ea9acd6fa17b8a6eccc3315c61c0407523b
Mario <mariocoelho-21@hotmail.com>
2024-08-04 10:06:06 -0300

    Add page and pageSize

--- Controllers/PostControllers.cs
+++ Controllers/PostControllers.cs
@@ -1,4 +1,6 @@
 using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,23 +12,42 @@ public class PostControllers : ControllerBase
 {
     [HttpGet("v1/posts")]
     public async Task<IActionResult> GetAsync(
-        [FromServices] BlogDataContext context)
+        [FromServices] BlogDataContext context,
+        [FromQuery]int page = 0,
+        [FromQuery]int pageSize = 25)
     {
-        var posts = await context
-            .Posts
-            .AsNoTracking()
-            .Include(x=>x.Category)
-            .Include(x=>x.Author)
-            .Select(x=>new ListPostsViewModel
+        try{
+            var count = await context.Posts.AsNoTracking().CountAsync();
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x=>x.Category)
+                .Include(x=>x.Author)
+                .Select(x=>new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title, 
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x=>x.LastUpdateDate)
+                .ToListAsync();
+            
+            return Ok(new ResultViewModel<dynamic>(new
             {
-                Id = x.Id,
-                Title = x.Title, 
-                Slug = x.Slug,
-                LastUpdateDate = x.LastUpdateDate,
-                Category = x.Category.Name,
-                Author = $"{x.Author.Name} ({x.Author.Email})"
-            })
-            .ToListAsync();
-        return Ok(posts);
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Internal Falid!"));
+        }
     }
 }
--- obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e089befa21099cdf194ff43c40b55ca3a09a4532")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e4102edf29e1725c6bbe230a0d6f00f5d4ba22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
