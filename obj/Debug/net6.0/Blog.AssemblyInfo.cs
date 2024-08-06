8bdef14af7f5e07e1bdea0f7d8caaf48225794c6
Mario <mariocoelho-21@hotmail.com>
2024-08-06 18:22:04 -0300

    add filter (url ex : "v1/category/backend")

--- obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e4102edf29e1725c6bbe230a0d6f00f5d4ba22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f724ea9acd6fa17b8a6eccc3315c61c0407523b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
